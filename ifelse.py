74234a6ebffd3488a13d7b328e251000e1e307e7
Kagome Miller <kagomemiller@gmail.com>
2021-02-11 18:28:50 -0600

    Add files via upload

---
+++ ifelse.py
@@ -0,0 +1,20 @@
+# write if/else to assign the smaller of a & b
+# ...to the variable m
+
+#example 1
+
+a = 100
+b = 50
+
+if a < b:
+    m = a
+else:
+    m = b
+
+
+#exampl 2
+
+a = 100
+b = 50
+
+m = a if a<b else b
